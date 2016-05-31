f1829eed5189d12ef11027bfe691cc706e9df969
nathan <nathan@dorkbox.com>
2016-05-31 02:31:47 +0200

    Updated copyright

--- src/dorkbox/console/Console.java
+++ src/dorkbox/console/Console.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 dorkbox, llc
+ * Copyright 2010 dorkbox, llc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
